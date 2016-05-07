c85541d8423cc91f39c1604432ba3474e2f83ed8
Marcin Baniowski <marcin.baniowski@performgroup.com>
2016-05-08 00:50:50 +0200

    container tests for EmployeeList

--- src/test/components/App.spec.js
+++ src/test/components/App.spec.js
@@ -5,10 +5,17 @@ import App from '../../components/App'
 import EmployeeList from '../../components/EmployeeList'
 import PositionFilter from '../../components/PositionFilter'
 
+let wrapper
+
 describe('App component', () => {
+  beforeEach(() => {
+    wrapper = shallow(<App/>);
+  })
   it('Should render the application', () => {
-    const wrapper = shallow(<App/>);
     expect(wrapper.find(PositionFilter).length).toEqual(1)
     expect(wrapper.find(EmployeeList).length).toEqual(1)
   });
+  it('Should contain 2 components', () => {
+    expect(wrapper.children().length).toEqual(2)
+  })
 });
--- src/test/components/EmployeeList.spec.js
+++ src/test/components/EmployeeList.spec.js
@@ -1,25 +1,26 @@
 import expect from 'expect'
 import React from 'react'
 import { shallow } from 'enzyme'
-import { EmployeeList } from '../../components/EmployeeList'
+import { EmployeeList, getEmployeeList } from '../../components/EmployeeList'
 
 let wrapper
+let employeeData = [
+  {
+    'id': 1,
+    'name': 'Reese Hardin',
+    'position': 'Web Developer'
+  },
+  {
+    'id': 2,
+    'name': 'Cora Ashley',
+    'position': 'Java Developer'
+  }
+]
 
 describe('EmployeeList component', () => {
   beforeEach(() => {
     let props = {
-      employees: [
-        {
-      		"id": 1,
-      		"name": "Reese Hardin",
-      		"position": "Web Developer"
-      	},
-      	{
-      		"id": 2,
-      		"name": "Cora Ashley",
-      		"position": "Java Developer"
-      	}
-      ]
+      employees: employeeData
     }
     wrapper = shallow(<EmployeeList {...props} />)
   })
@@ -33,4 +34,22 @@ describe('EmployeeList component', () => {
   it('Should render employee data in the second li as text', () => {
     expect(wrapper.find('li').at(1).text()).toEqual('Cora Ashley - Java Developer')
   });
+
+  describe('Filtering employee list', () => {
+    it('Should return the whole list if the filter is not specified', () => {
+      expect(getEmployeeList(employeeData, null)).toEqual(employeeData)
+    })
+    it('Should filter the list by a filter', () => {
+      expect(getEmployeeList(employeeData, 'Java Developer')).toEqual(
+        [
+          {
+            'id': 2,
+            'name': 'Cora Ashley',
+            'position': 'Java Developer'
+          }
+        ]
+      )
+    })
+  })
+
 });
