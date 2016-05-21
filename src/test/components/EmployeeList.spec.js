cfac56df598e7a9f723360b278348fe039f1d6ef
Marcin Baniowski <marcin.baniowski@performgroup.com>
2016-05-21 12:20:23 +0200

    unit tests modification

--- src/test/components/EmployeeList.spec.js
+++ src/test/components/EmployeeList.spec.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme'
 import { EmployeeList, getEmployeeList } from '../../components/EmployeeList'
 
 let wrapper
-let employeeData = [
+const employeeData = [
   {
     'id': 1,
     'name': 'Reese Hardin',
@@ -24,32 +24,37 @@ describe('EmployeeList component', () => {
     }
     wrapper = shallow(<EmployeeList {...props} />)
   })
+  it('Should not contain li elements when collection is empty', () => {
+    let props = {employees: []}
+    const emptyWrapper = shallow(<EmployeeList {...props} />)
+    expect(emptyWrapper.find('ul').length).toEqual(1)
+    expect(emptyWrapper.find('li').length).toEqual(0)
+  })
   it('Should render the ul and li elements', () => {
     expect(wrapper.find('ul').length).toEqual(1)
     expect(wrapper.find('li').length).toEqual(2)
-  });
+  })
   it('Should render employee data in the first li as text', () => {
     expect(wrapper.find('li').first().text()).toEqual('Reese Hardin - Web Developer')
-  });
+  })
   it('Should render employee data in the second li as text', () => {
     expect(wrapper.find('li').at(1).text()).toEqual('Cora Ashley - Java Developer')
-  });
-
-  describe('Filtering employee list', () => {
-    it('Should return the whole list if the filter is not specified', () => {
-      expect(getEmployeeList(employeeData, null)).toEqual(employeeData)
-    })
-    it('Should filter the list by a filter', () => {
-      expect(getEmployeeList(employeeData, 'Java Developer')).toEqual(
-        [
-          {
-            'id': 2,
-            'name': 'Cora Ashley',
-            'position': 'Java Developer'
-          }
-        ]
-      )
-    })
   })
+})
 
-});
+describe('Filtering employee list', () => {
+  it('Should return the whole list if the filter is not specified', () => {
+    expect(getEmployeeList(employeeData, null)).toEqual(employeeData)
+  })
+  it('Should filter the list by a filter', () => {
+    expect(getEmployeeList(employeeData, 'Java Developer')).toEqual(
+      [
+        {
+          'id': 2,
+          'name': 'Cora Ashley',
+          'position': 'Java Developer'
+        }
+      ]
+    )
+  })
+})
